e9a5f1bfab11e817c25d5f7766f34e45d8f631d7
sossw1 <sossw1@gmail.com>
2021-03-07 15:58:48 -0500

    Mongoose connection

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // app.use(require("./routes/api.js"));
 
