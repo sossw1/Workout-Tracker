aba54c4f596f65d06d166599e42cf4f764089b6b
sossw1 <sossw1@gmail.com>
2021-03-07 18:27:28 -0500

    Added routes

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useFindAndModify: false
 });
 
-// app.use(require("./routes/api.js"));
+app.use(require("./routes/api-routes"));
+app.use(require("./routes/html-routes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
