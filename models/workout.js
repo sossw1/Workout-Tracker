8e305f00b14029fe3ebcf1632edf842e1cf8feeb
sossw1 <sossw1@gmail.com>
2021-03-07 17:11:08 -0500

    Fixed structure

--- models/workout.js
+++ models/workout.js
@@ -2,35 +2,38 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for workout"
-  },
-  date: {
+  day: {
     type: Date,
     default: Date.now
   },
-  type: {
-    type: String,
-  },
-  weight: {
-    type: Number,
-    default: 0
-  },
-  sets: {
-    type: Number,
-    default: 0
-  },
-  reps: {
-    type: Number,
-    default: 0
-  },
+  exercises: [
+    {
+      name: {
+        type: String,
+        trim: true,
+      },
+      type: {
+        type: String,
+      },
+      weight: {
+        type: Number,
+        default: 0
+      },
+      sets: {
+        type: Number,
+        default: 0
+      },
+      reps: {
+        type: Number,
+        default: 0
+      },
+      distance: {
+        type: Number,
+        default: 0
+      }
+    }
+  ],
   duration: {
-    type: Number,
-    default: 0
-  },
-  distance: {
     type: Number,
     default: 0
   }
