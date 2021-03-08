cf4d7f77fa3e528b909161a70700afc22ec54207
sossw1 <sossw1@gmail.com>
2021-03-07 22:02:45 -0500

    PUT api/workouts/id

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { db } = require('../models/workout.js');
 const Workout = require("../models/workout.js");
 
 router.get("/api/workouts", (req, res) => {
@@ -12,13 +13,23 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
-router.post("api/workouts", (req, res) => {
+router.post("/api/workouts", (req, res) => {
   Workout.create(req.body)
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findOneAndUpdate({_id: req.params.id}, { $push: {exercises: req.body}}, {new: true})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
     });
 });
 
