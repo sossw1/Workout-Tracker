fefbfe6ab25eb30d8e54a9e1d772b75c0c3e0187
sossw1 <sossw1@gmail.com>
2021-03-07 22:04:27 -0500

    function workoutDuration

--- public/stats.js
+++ public/stats.js
@@ -7,7 +7,7 @@ function generatePalette() {
     '#d45087',
     '#f95d6a',
     '#ff7c43',
-    'ffa600',
+    '#ffa600',
     '#003f5c',
     '#2f4b7c',
     '#665191',
@@ -15,14 +15,14 @@ function generatePalette() {
     '#d45087',
     '#f95d6a',
     '#ff7c43',
-    'ffa600',
+    '#ffa600',
   ];
 
   return arr;
 }
 
 function populateChart(data) {
-  let durations = data.map(({ totalDuration }) => totalDuration);
+  let durations = workoutDuration(data);
   let pounds = calculateTotalWeight(data);
   let workouts = workoutNames(data);
   const colors = generatePalette();
@@ -189,6 +189,17 @@ function calculateTotalWeight(data) {
   });
 
   return totals;
+}
+
+function workoutDuration(data) {
+  let durations = [];
+
+  data.forEach((workout) => {
+    workout.exercises.forEach((exercise) => {
+      durations.push(exercise.duration);
+    });
+  });
+  return durations;
 }
 
 function workoutNames(data) {
